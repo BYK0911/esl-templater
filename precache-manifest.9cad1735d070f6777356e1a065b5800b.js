1f3432ebb64b7672c390442aabae5c9c24c73cb7
baoyukun <yukun.bao@hanshow.com>
2021-08-05 15:27:43 +0800

    build

---
+++ precache-manifest.9cad1735d070f6777356e1a065b5800b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "492a7761e496a1b4bab3",
+    "url": "css/app.890fad27.css"
+  },
+  {
+    "revision": "c905f5e65c9107089204",
+    "url": "css/chunk-vendors.a201866b.css"
+  },
+  {
+    "revision": "9f7baeb94be0ed4282cd",
+    "url": "css/home.81817859.css"
+  },
+  {
+    "revision": "2f55e0d4b3f9eb3ffaefdac379fa3f8b",
+    "url": "fonts/Poppins-Bold.2f55e0d4.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "060bf08d4e1f4849e809ed11ef413fdf",
+    "url": "fonts/iconfont.060bf08d.woff2"
+  },
+  {
+    "revision": "27fd0bb62487e2ccdd9a6816eabbf632",
+    "url": "fonts/iconfont.27fd0bb6.woff"
+  },
+  {
+    "revision": "66710f04bcddfbec05d3667b6574caa5",
+    "url": "fonts/iconfont.66710f04.ttf"
+  },
+  {
+    "revision": "27b51b818e5cd3a227de43445776bcd3",
+    "url": "index.html"
+  },
+  {
+    "revision": "492a7761e496a1b4bab3",
+    "url": "js/app.58ceaafa.js"
+  },
+  {
+    "revision": "c905f5e65c9107089204",
+    "url": "js/chunk-vendors.a06321c9.js"
+  },
+  {
+    "revision": "9f7baeb94be0ed4282cd",
+    "url": "js/home.62ca8aa7.js"
+  },
+  {
+    "revision": "339a2e85b3c479443b756a4949e132ea",
+    "url": "manifest.json"
+  }
+]);
